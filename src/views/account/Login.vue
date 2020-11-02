541f06dfef5ce62e1d8f45a001e55ef150a52c11
clone <bjbs08@qq.com>
2020-11-02 19:33:59 +0800

    页面布局

---
+++ src/views/account/Login.vue
@@ -0,0 +1,39 @@
+<template>
+ <div class="account">
+    <div class="form-wrap">
+      <a-form :form="form">
+        <a-form-item>
+          <label class="color-white">用户名</label>
+          <a-input type="text" autocomplete="off"/>
+        </a-form-item>
+        <a-form-item>
+          <label class="color-white">密码</label>
+          <a-input type="password" autocomplete="off"/>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" html-type="submit" @click="check">登录</a-button>
+        </a-form-item>
+      </a-form>
+      <div class="text-center font-size-12">
+        <a href="#" class="color-white">忘记密码</a> | <a href="#" class="color-white">注册</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, Ref,ref } from 'vue';
+export default defineComponent({
+  name: 'Login',
+  setup() {
+    //
+    const count = ref<number>(10)
+    console.error(count.value)
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./style.scss";
+</style>
+
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+module.exports = {
+  // 基本路径
+  publicPath: process.env.NODE_ENV === 'production' ? '' : './',
+  // 输出文件目录
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
+  // eslint-loader 是否在保存的时候检查
+  lintOnSave: false,
+  // css相关配置
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: true,
+    // 开启 CSS source maps?
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/main.scss";`
+      }
+    }
+  }
+}
